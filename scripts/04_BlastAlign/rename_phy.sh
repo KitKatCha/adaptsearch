7ac2f82493772297bfd5a51913673b6330ed5837
Mataivic <victor.mataigne@wanadoo.fr>
2017-10-26 11:36:21 +0200

    wip for files renaming

--- scripts/04_BlastAlign/rename_phy.sh
+++ scripts/04_BlastAlign/rename_phy.sh
@@ -2,10 +2,14 @@
 # Author : Victor Mataigne (ABiMS, Station Biologique de Roscoff)
 # A small script which rename a file correctly, according to its number of sequences
 
+# $1 : locusX_spY.fasta.phy
+# $2 : locusX_spY.fasta.nxs
+
 name=$(echo $1 | cut -d _ -f 1)
 under=$(echo _sp)
-number=$(grep '/' $1 | wc -l)  
+number=$(grep '/' $1 | wc -l) 
 phy=$(echo .phy)
-new_file=$name$under$number$phy
-cat $1 > $new_file
-ln -s $new_file $2
+new_file=$name$under$number".phy"
+mv $1 new_file
+new_file=$name$under$number".nxs"
+mv $2 new_file
